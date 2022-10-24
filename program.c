ddb475bea58f6e492d9427000f07ed47a5163a9c
risobak <nicolasobakke@gmail.com>
2022-10-24 20:18:12 +0200

    Add minor improvements

--- program.c
+++ program.c
@@ -18,40 +18,29 @@ int read_file(char text[], char *filename);
 
 int main()
 {
-    char symbols[] = "½§!@#£¤$%%&/{([)]=}?+`´|\\><;,:.-_¨^~'*";
-    
-    Doc user_doc;
-    Doc source_doc;
-    strcpy(user_doc.text, "This is some text that we'll check for plagiarism");
-    strcpy(source_doc.text, "This is some source text that we'll compare with the user text");
-    remove_character(user_doc.text, symbols);
-    remove_character(source_doc.text, symbols);
-    split_words(&user_doc);
-    split_words(&source_doc);
     check_plagiarism();
     return 0;
 }
 
-void remove_character(char *str, char *remove){
+void remove_character(char *str, char *remove)
+{
     // copy characters, but only increment destination for "non-remove" characters
-    // dst = destination, src = source
     // strchr: Returns a pointer to the first occurrence of the character "str[dst]" in the string remove, or NULL if the character is not found
-    for(size_t src = 0, dst = 0; (str[dst] = str[src]) != '\0'; src++)
+    for (size_t src = 0, dst = 0; (str[dst] = str[src]) != '\0'; src++)
         dst += (strchr(remove, str[dst]) == NULL);
-    
 }
 
 void check_plagiarism()
 {
+    char symbols[] = "½§!@#£¤$%%&/{([)]=}?+`´|\\><;,:.-_¨^~'*";
     Doc user_doc;
     Doc source_doc;
     read_file(user_doc.text, "user_doc.txt");
     read_file(source_doc.text, "source_doc.txt");
-    //strcpy(user_doc.text, "This is some text that we'll check for plagiarism");
-    //strcpy(source_doc.text, "This is some source text that we'll compare with the user text");
+    remove_character(user_doc.text, symbols);
+    remove_character(source_doc.text, symbols);
     split_words(&user_doc);
     split_words(&source_doc);
-    check_quotations();
     compare(&user_doc, source_doc);
 }
 
@@ -114,18 +103,22 @@ void compare(Doc *user_doc, Doc source_doc)
     printf("\n\n");
 }
 
-int read_file(char text[], char *filename) {
-    
-    FILE * doc; 
+int read_file(char text[], char *filename)
+{
+
+    FILE *doc;
 
     // Opening file, set to read mode and checking if possible to open file
     doc = fopen(filename, "r");
-    if (doc == NULL) { printf("Could not open file!"); return -1; }
+    if (doc == NULL)
+    {
+        printf("Could not open file!");
+        return -1;
+    }
 
     // Read the file content until either end of file or reached the maximum characters
-    while(!feof(doc)) {
+    while (!feof(doc))
         fgets(text, 100, doc);
-    }
 
     fclose(doc);
 
