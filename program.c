52d517132daebc174c20e0a51cd62636d2dff843
risobak <nicolasobakke@gmail.com>
2022-11-26 19:33:41 +0100

    Resolve #9; add semantic function
    Each time words are not equal to eachother then check for semantic similarities by looping through a 2d string array containing synonyms

--- program.c
+++ program.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#define SYNONYM_COLS 5
 
 typedef struct Document
 {
@@ -9,20 +10,27 @@ typedef struct Document
     int words_len, og_words_len, cited_words[200], cited_words_len;
 } Doc;
 
+static int language = 1;
+static const char *en_synonyms[][SYNONYM_COLS] = {{"this", "that", ""}, {"like", "alike", "comparable", "related", ""}};
+static const char *dk_synonyms[][SYNONYM_COLS] = {{"men", "alligevel", "derfor", "dog", "endda"}, {"og", "plus", "samt", ""}};
+
+void flush_stdin();
 void check_plagiarism();
 void split_words(Doc *doc);
 void find_quotations(Doc *doc);
 void free_struct_vars(Doc *doc);
-void remove_character(char *str);
 int is_quoted(Doc doc, int index);
 void read_file(Doc *doc, char *filename);
 void compare(Doc *user_doc, Doc source_doc);
+int check_similarity(char *word1, char *word2);
 void delete_dublicates(int arr[], int *length);
+void remove_characters(char *str, char *letters);
 void split(char **arr[], char *text, int *length);
 int comparator(const void *num_1, const void *num_2);
 void readd_symbols(Doc doc, int similar[], int sim_len);
 void get_file_names(char *user_file, char *source_files);
 void print_result(Doc doc, int similar[], float percent, int user_len);
+int check_synonyms(char *word1, char *word2, const char *synonyms[][SYNONYM_COLS], int rows);
 
 int main()
 {
@@ -44,6 +52,7 @@ void check_plagiarism()
     }
     split_words(&user_doc);
     split_words(&source_doc);
+    check_similarity("char *word1", "char *word2");
     find_quotations(&user_doc);
     compare(&user_doc, source_doc);
     free_struct_vars(&user_doc);
@@ -52,18 +61,29 @@ void check_plagiarism()
 
 void get_file_names(char *user_file, char *source_files)
 {
-    char temp;
+    int i = 0, lang_len = 2, similar;
+    printf("What language is your files written in? \033[0;30m[1]\x1b[0m English  \033[0;30m[2]\x1b[0m Danish: ");
+    if (!scanf("%d", &language) || lang_len - language < 0)
+        language = 1;
+    flush_stdin();
     printf("Input your filename: ");
     scanf("%s", user_file);
-    scanf("%c", &temp); // clears buffer
-    printf("Input the source filenames (divide mulitple filenames with a space): ");
+    flush_stdin();
+    printf("Input source filenames (divide mulitple filenames with a space): ");
     scanf("%[^\n]", source_files);
     // In development you should uncomment the two lines below and comment the lines above so you don't need to give an input all the time. !REMEMBER! to revert it back when pushing the code to github
     // strcpy(user_file, "user.txt");
     // strcpy(source_files, "src_1.txt src_2.txt");
 }
 
-void remove_character(char *str)
+// Remove buffer stdin values
+void flush_stdin()
+{ // clang-format off
+    int c;
+    while ((c = fgetc(stdin)) != '\n' && c != EOF);
+} // clang-format on
+
+void remove_characters(char *str, char *letters)
 {
     // Increment all "non-symbols"
     // strchr: Returns a pointer to the first occurrence of the character "str[dst]" in the string symbols, or NULL if the character is not found
@@ -72,7 +92,6 @@ void remove_character(char *str)
     // vi kører for-løkken og fordi (str[dst] = str[src]), hvilket er (0 = 1), så bliver src incrementet altså [H, 0, e, j], og dst = 1 fordi det ikke er et symbol og src++
     // str[dst] "1" = str[src] "2" = [H, e, 0, j], dst = 2, src++
     // str[dst] "2" = str[src] "3" = [H, e, j, 0], dst = 3, src++ og til sidst rammer vi \0 og løkken afsluttes
-    char letters[] = "abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ ";
 
     for (size_t src = 0, dst = 0; (str[dst] = tolower(str[src])) != '\0'; src++)
         dst += (strchr(letters, str[dst]) != NULL);
@@ -82,11 +101,16 @@ void remove_character(char *str)
 // The words and length gets stored in the struct output paramater
 void split_words(Doc *doc)
 {
+    char letters[40] = "abcdefghijklmnopqrstuvwxyz ";
+    char dk_letters[] = "æøå";
+    if (language == 2)
+        strcat(letters, dk_letters);
+
     // str token destroys the orignal string and breaks it into smaller strings and returns a pointer to them
     // There needs to be two different text variables since strtok returns the pointer to the modified string
     // A local scope text variable wouldn't suffice since it would be deleted after function run
     split(&doc->og_words, doc->og_text, &doc->og_words_len);
-    remove_character(doc->text);
+    remove_characters(doc->text, letters);
     split(&doc->words, doc->text, &doc->words_len);
 }
 
@@ -159,22 +183,22 @@ void compare(Doc *user_doc, Doc source_doc)
     {
         for (j = 0; j < source_len; j++)
         {
-            if (!strcmp(user_doc->words[i], source_doc.words[j]))
+            if (check_similarity(user_doc->words[i], source_doc.words[j]))
             {
                 similar = 1;
 
                 // Checks similarities of the following words
                 while (i + similar < user_len &&
                        j + similar < source_len &&
-                       !strcmp(user_doc->words[i + similar], source_doc.words[j + similar]))
+                       check_similarity(user_doc->words[i + similar], source_doc.words[j + similar]))
                     similar++;
 
                 // Check if the word(s) is an extension to other similarities
                 if (similar <= 2 && count && j - 1 >= 0)
                 {
-                    strcmp_val = !strcmp(user_doc->words[similarities[count - 1]], source_doc.words[j - 1]);
+                    strcmp_val = check_similarity(user_doc->words[similarities[count - 1]], source_doc.words[j - 1]);
                     if (strcmp_val && similar == 1 && j - 2 >= 0)
-                        strcmp_val = !strcmp(user_doc->words[similarities[count - 2]], source_doc.words[j - 2]);
+                        strcmp_val = check_similarity(user_doc->words[similarities[count - 2]], source_doc.words[j - 2]);
                 }
 
                 // If theres 3 or more identical words in a row then store the indexes of the words
@@ -219,6 +243,46 @@ void compare(Doc *user_doc, Doc source_doc)
                      (float)count / source_len * 100, // <- Percent
                      source_doc.og_words_len);
     }
+}
+
+int check_similarity(char *word1, char *word2)
+{
+    int result = !strcmp(word1, word2);
+    if (!result)
+    {
+        int rows, size;
+        if (language == 2)
+        {
+            rows = sizeof(dk_synonyms) / sizeof(dk_synonyms[0]);
+            result = check_synonyms(word1, word2, dk_synonyms, rows);
+        }
+        else
+        {
+            rows = sizeof(en_synonyms) / sizeof(en_synonyms[0]);
+            result = check_synonyms(word1, word2, en_synonyms, rows);
+        }
+    }
+    return result;
+}
+
+int check_synonyms(char *word1, char *word2, const char *synonyms[][SYNONYM_COLS], int rows)
+{
+    int i, j = 0, result = 0;
+
+    for (i = 0; i < rows; i++)
+        for (j = 0; j < SYNONYM_COLS && en_synonyms[i][j]; j++)
+            if (!strcmp(en_synonyms[i][j], word1))
+            {
+                j = 0;
+                do
+                {
+                    result = !strcmp(en_synonyms[i][j], word2);
+                    if (result)
+                        return result;
+                } while (en_synonyms[i][++j]);
+                return result;
+            }
+    return result;
 }
 
 // Change similarity values to reflect the right index in the unedited words array.
@@ -283,12 +347,10 @@ void print_result(Doc doc, int similar[], float percent, int len)
     if (run)
         strcpy(title, "SOURCE ");
 
-    printf("\n--------------------------------\n%s%s"
-           "PLAGIARISED TEXT (%.1f%%):\x1b[0m"
-           "\n--------------------------------\n",
-           color, title, percent);
-
-    printf("\033[0;30m%s\x1b[0m\n", doc.filenames[0]);
+    printf("\n\033[0;30m--------------------------------\n%s%s"
+           "PLAGIARISED TEXT (%.1f%%):"
+           "\n\033[0;30m--------------------------------\n%s\x1b[0m\n",
+           color, title, percent, doc.filenames[0]);
 
     for (int i = 0; i < len; i++)
     {
